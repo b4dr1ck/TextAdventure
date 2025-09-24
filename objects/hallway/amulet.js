ffde84f12dd838d822138d406a344b72306df8cc
Patrick Reiter <patrick.reiter@itsv.at>
2025-09-24 18:13:29 +0200

    EXTEND: amulet.js: amulet-trigger wenn man es ausruestet

--- objects/hallway/amulet.js
+++ objects/hallway/amulet.js
@@ -1,10 +1,22 @@
 import { Equipment } from "../../classes.js";
+import { player } from "../../init.js";
 
 export const amulet = new Equipment(
-  "amulet",
+  "strange amulet",
   "amulet1",
-  ["amulet", "golden amulet", "necklace"],
-  "A beautiful golden amulet with a glowing blue gem in the center.<br>It looks very valuable."
+  ["amulet", "silver amulet", "necklace", "strange amulet"],
+  "A strange silver amulet with a black gem in the center that shows a skull engraving."
 );
 amulet.canTake = true;
 amulet.canThrow = true;
+
+const amuletWearTrigger = () => {
+  player.adjustHealth(-100);
+  return (
+    "You put on the amulet.<br>Out of the sudden, you feel a surge of dark energy coursing through your veins.<br>" +
+    "Your skin begins to get paler, and your eyes darken.<br>" +
+    "Seconds later you fall to the ground..."
+  );
+};
+
+amulet.createTrigger("dress", amuletWearTrigger);
